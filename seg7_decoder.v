169621bf3dbc1f074a2d2c3ca43c5d5ab87af4e9
Lee Seo Young <163075427+2seoyoungg@users.noreply.github.com>
2026-06-03 20:16:04 +0900

    Update seg7_decoder.v

--- seg7_decoder.v
+++ seg7_decoder.v
@@ -1,4 +1,3 @@
-
 module seg7_decoder (
     input  wire [3:0] digit_in,
     output reg  [7:0] seg_out
@@ -6,22 +5,25 @@ module seg7_decoder (
 
     always @* begin
         case (digit_in)
-            4'h0 : seg_out = 8'b11111100;  // 0
-            4'h1 : seg_out = 8'b01100000;  // 1
-            4'h2 : seg_out = 8'b11011010;  // 2
-            4'h3 : seg_out = 8'b11110010;  // 3
-            4'h4 : seg_out = 8'b01100110;  // 4
-            4'h5 : seg_out = 8'b10110110;  // 5
-            4'h6 : seg_out = 8'b10111110;  // 6
-            4'h7 : seg_out = 8'b11100000;  // 7
-            4'h8 : seg_out = 8'b11111110;  // 8
-            4'h9 : seg_out = 8'b11110110;  // 9
-            4'hA : seg_out = 8'b11111110;  
-            4'hB : seg_out = 8'b00000010;  // '-'
-            4'hC : seg_out = 8'b00000000;  // ' '
-            4'hD : seg_out = 8'b10001110;  // 'F'
-            4'hE : seg_out = 8'b11101110;  // 'A'
-            4'hF : seg_out = 8'b00011100;  // 'L'
+            4'h0 : seg_out = 8'b11111100;
+            4'h1 : seg_out = 8'b01100000;
+            4'h2 : seg_out = 8'b11011010;
+            4'h3 : seg_out = 8'b11110010;
+            4'h4 : seg_out = 8'b01100110;
+            4'h5 : seg_out = 8'b10110110;
+
+            4'h6 : seg_out = 8'b00101010;  // N
+            4'h7 : seg_out = 8'b10011110;  // E
+            4'h8 : seg_out = 8'b11111100;  // O
+            4'h9 : seg_out = 8'b11001110;  // P
+
+            4'hA : seg_out = 8'b11111110;  // *
+            4'hB : seg_out = 8'b00000010;  // -
+            4'hC : seg_out = 8'b00000000;  // blank
+            4'hD : seg_out = 8'b10001110;  // F
+            4'hE : seg_out = 8'b11101110;  // A
+            4'hF : seg_out = 8'b00011100;  // L
+
             default: seg_out = 8'b00000000;
         endcase
     end
